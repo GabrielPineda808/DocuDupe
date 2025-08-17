c08ce45ee6cb0ed238b8be2c3d813f4163d5fea6
Gabriel Pineda <gabepineda6@gmail.com>
2025-08-17 00:04:49 -0700

    first commit

---
+++ src/main/java/com/example/DocuDupe/DocuDupeApplication.java
@@ -0,0 +1,13 @@
+package com.example.DocuDupe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuDupeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocuDupeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DocuDupe/DocuDupeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DocuDupe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuDupeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
